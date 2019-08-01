1cd43ea53916206c884b54c0fdf1eba8b55b5d5b
Aruncsk <arunsubburaj@gmail.com>
2019-08-01 17:22:16 +0530

    Azure Scripts

---
+++ AzureDataFactory/TwitterSentiment.py
@@ -0,0 +1,32 @@
+from tweepy.streaming import StreamListener
+from tweepy import OAuthHandler
+from tweepy import Stream
+import json
+import pandas as pd
+import matplotlib.pyplot as plt
+
+access_token = ""
+access_token_secret = ""
+consumer_key = ""
+consumer_secret = ""
+
+class StdOutListener(StreamListener):
+
+    def on_data(self, data):
+        print(data)
+        return True
+
+    def on_error(self, status):
+        print(status)
+
+
+if __name__ == '__main__':
+
+    #This handles Twitter authetification and the connection to Twitter Streaming API
+    l = StdOutListener()
+    auth = OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_token, access_token_secret)
+    stream = Stream(auth, l)
+
+    #This line filter Twitter Streams to capture data by the keywords: 'python', 'javascript', 'ruby'
+    stream.filter(track=['python', 'javascript', 'ruby'])
